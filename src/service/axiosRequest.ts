ac0830d18b44e623ce33e1502c92da839b966c00
MohammedSwaaf <mohammedswaaf@gmail.com>
2023-08-11 15:50:38 +0300

    Change the token because the last one is expired

--- src/service/axiosRequest.ts
+++ src/service/axiosRequest.ts
@@ -11,7 +11,7 @@ export const request = ({
     method,
     params,
     headers: {
-      Authorization: `Bearer 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`,
+      Authorization: `Bearer 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`,
       ...headers,
     },
   };
